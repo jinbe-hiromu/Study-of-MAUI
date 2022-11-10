c3ce4690a9eade20afe175a2093442edb1a8fe2e
HIROMU MATSUBARA <hiromu.matsubara.j2x@denso-wave.com>
2022-11-10 16:48:12 +0900

    add delegateCommand BindableBase

---
+++ MauiTestApp/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace MauiTestApp
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _action;
+
+        public DelegateCommand(Action action)
+        {//コンストラクタでActionを登録
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {//とりあえずActionがあれば実行可能
+            return _action != null;
+        }
+
+        public void Execute(object parameter)
+        {//今回は引数を使わずActionを実行
+            _action?.Invoke();
+        }
+    }
+}
---
+++ MauiTestApp/TextPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiTestApp
+{
+    public class TextPageViewModel  : BindableBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
+        public string Address { get; set; } = string.Empty;
+
+        public string _result = string.Empty;
+        public string Result
+        {
+            get => _result;
+            set => SetProperty(ref _result, value, nameof(Result));
+        }
+
+        private DelegateCommand _enterCommand;
+
+        public DelegateCommand EnterCommand
+        {
+            get
+            {
+                return _enterCommand ?? (_enterCommand = new DelegateCommand(() => { Result = $"{Name} {Age} {Address}"; }));
+            }
+        }
+    }
+
+}
